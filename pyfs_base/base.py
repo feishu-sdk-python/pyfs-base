521447aa9d993eb03a329e4d88fab65f40e4bf44
huangqimin001 <huangqimin001@ke.com>
2020-09-15 15:13:33 +0800

    :art: Authorization Bearer <app_access_token>

--- pyfs_base/base.py
+++ pyfs_base/base.py
@@ -26,20 +26,23 @@ class BaseFeishu(object):
         resjson = res.json()
         return resjson_processor_func(resjson) if resjson_processor_func else resjson
 
-    def post(self, url, verify=False, encoding='utf-8', data_to_json_str=True, data_to_xml_str=False, res_to_encoding=True, res_to_json=True, res_processor_func=None, resjson_processor_func=None, **kwargs):
+    def post(self, url, verify=False, encoding='utf-8', data_to_json_str=True, data_to_xml_str=False, res_to_encoding=True, res_to_json=True, res_processor_func=None, resjson_processor_func=None, authorization=True, authorization_key='token', **kwargs):
         # https://github.com/requests/requests/blob/9dd823c289faca0d496ef71f25d36216d2259ca3/requests/models.py#L87
         # if (not files):
         #     raise ValueError("Files must be provided.")
         # elif isinstance(data, basestring):
         #     raise ValueError("Data must not be a string.")
         if not kwargs.get('files', None):
-            if data_to_json_str:
-                data = kwargs.get('data', None)
-                if data and isinstance(data, dict):
+            data = kwargs.get('data', None)
+            if data:
+                if authorization and isinstance(data, dict) and data.get(authorization_key):
+                    kwargs['headers'] = {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer {0}'.format(data.get(authorization_key))
+                    }
+                if data_to_json_str and isinstance(data, dict):
                     kwargs['data'] = json.dumps(data, ensure_ascii=False).encode('utf-8')
-            if data_to_xml_str:
-                data = kwargs.get('data', None)
-                if data and isinstance(data, dict):
+                if data_to_xml_str and isinstance(data, dict):
                     kwargs['data'] = dict_to_xml(data)
         res = requests.post(url, verify=verify, **kwargs)
         if res_to_encoding:
